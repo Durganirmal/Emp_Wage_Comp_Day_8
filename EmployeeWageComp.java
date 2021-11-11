7050860f855eb9a6ea703aa105ba73d2c5e344e6
Durga Nirmal <durganirmal123@gmail.com>
2021-11-11 16:53:35 +0530

    Added :Program for Refactor the Program add the method

--- EmployeeWageComp.java
+++ EmployeeWageComp.java
@@ -11,7 +11,10 @@ public class EmployeeWageComp {
 
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee Wage Computation Program !!");
+        System.out.println("Welcome to Employee Wage Computation Program !!");
+        EmployeeWageComp();
+    }
+    public static void EmployeeWageComp(){
         int workingDay = 20;
         int empHrs = 0;
         int empwage = 0;
@@ -40,7 +43,7 @@ public class EmployeeWageComp {
             totalWage = totalWage + empwage;
             System.out.println("Total working hrs = " + totalWorkingHrs);
             System.out.println("Total Emp wage for 20 days = " + totalWage);
-        }
+          }
 
-    }
-}
+       }
+   }
